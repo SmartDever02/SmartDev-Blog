87ecd29ace296dfd85450de51d66ddfc2f2f53bc
SmartDever02 <smart.jamesjin@gmail.com>
2022-06-02 14:13:00 -0400

    Create React18 App with Vite & Tailwind

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className='App flex justify-center items-center min-h-screen'>
+      <div className='w-[400px] h-[400px] bg-[#232536] rounded-xl'></div>
+    </div>
+  );
+}
+
+export default App;
